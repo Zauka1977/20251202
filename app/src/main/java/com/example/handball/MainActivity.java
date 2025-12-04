4122134403fe3aafa77fdd94160a18ce532e1d7d
Zauka1977 <g.zauka@gmail.com>
2025-12-04 18:49:08 +0100

    Add files via upload

--- app/src/main/java/com/example/handball/MainActivity.java
+++ app/src/main/java/com/example/handball/MainActivity.java
@@ -1,10 +1,10 @@
+
 package com.example.handball;
 
-import android.app.Activity; // Base Activity
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity { // EXTENDS Activity!
-
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
