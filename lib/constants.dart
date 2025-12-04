7cc826b65892d3f48935bd20cf4108ccdb3543be
Zauka1977 <g.zauka@gmail.com>
2025-12-04 17:13:01 +0100

    Create constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+const String APP_TITLE = 'Kézilabda mérkőzés Statisztika';
+const String EXPORT_HEADER = 'Simontornya KK LU14. 2025';
+final List<String> DEFAULT_STAT_FIELDS = [
+  'Gól','Lövés','Kapura lövés','Mellé','Blokk','Hetes_bedobott','Hetes_kihagyott',
+  'Védések','Kiállítás','Sárga','Piros','Labdaszerzés','Eladott labda','Megjegyzés'
+];
